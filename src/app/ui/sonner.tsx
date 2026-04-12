088dea11bd57c347876653cb0e010881a269b832
DoSafills <ricardo.erest@gmail.com>
2026-04-12 19:27:22 -0400

    Add files via upload

---
+++ src/app/ui/sonner.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Toaster as Sonner, ToasterProps } from "sonner";
+
+const Toaster = ({ ...props }: ToasterProps) => {
+  return (
+    <Sonner
+      theme="light"
+      className="toaster group"
+      style={
+        {
+          "--normal-bg": "#ffffff",
+          "--normal-text": "#000000",
+          "--normal-border": "#e5e5e5",
+        } as React.CSSProperties
+      }
+      {...props}
+    />
+  );
+};
+
+export { Toaster };
