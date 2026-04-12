50d0ac7328ecb2ba0a1dca781c3d0bb751e7b257
DoSafills <ricardo.erest@gmail.com>
2026-04-12 19:09:26 -0400

    Update routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
-import UserTicket from "../app/components/UserTicket";
-import TechPanel from "../app/components/TechPanel";
+import UserTicket from "./app/components/UserTicket";
+import TechPanel from "./app/components/TechPanel";
 
 export const router = createBrowserRouter([
   {
